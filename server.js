e7cad40ab92001c17dd9654a6ce88ec4725f4a8e
Marta Majerowska <majeranek85@gmail.com>
2020-07-21 21:59:09 +0200

    Add server side sockets

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const socket = require('socket.io');
 
+const tasks = [];
+
 const app = express();
 
 // Serve static files from the React app
@@ -20,3 +22,23 @@ app.use((req, res) => {
 });
 
 const io = socket(server);
+
+io.on('connection', socket => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', newTask => {
+    tasks.push(newTask);
+    socket.broadcast.emit('addTask', newTask);
+    console.log(tasks);
+  });
+
+  socket.on('removeTask', data => {
+    console.log(`remove task of id=${data}`);
+    const getTask = tasks.find(task => task.id == data);
+    const taskIndex = tasks.indexOf(getTask);
+    tasks.splice(taskIndex,1);
+    socket.broadcast.emit('removeTask', data);
+    console.log(tasks);
+  });
+});
