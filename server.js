33ec9f0adc0a6d680d2220bed39135eef0272bed
Marta Majerowska <majeranek85@gmail.com>
2020-07-21 07:14:55 +0200

    Add server and client side starting files

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const socket = require('socket.io');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running...');
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
+
+const io = socket(server);
